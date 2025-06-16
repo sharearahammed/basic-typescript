836ae3ea7d261a43505f6fb0351b99edf3d20a0c
sharearahammed <sharearahammed@gmail.com>
2025-06-16 20:43:18 +0600

    classes

--- src/index.ts
+++ src/index.ts
@@ -138,16 +138,39 @@
 // classes
 
 class Player {
+  public readonly id: string;
   constructor(
     private height: number,
     public weight: number,
     protected power?: number
-  ) {}
-  getMyHeight = () => {
+  ) {
+    this.id = String(Math.random() * 10);
+  }
+  // getMyHeight = () => {
+  //   return this.height;
+  // };
+  get getMyHeight ()  {
     return this.height;
   };
 }
-
-const nihal = new Player(20, 12 );
+const nihal = new Player(20, 12, 30);
 // nihal.height
-console.log(nihal.getMyHeight());
+// class Player2 extends Player {
+//   special: boolean;
+//   constructor(height: number, weight: number, power: number, special: boolean) {
+//     super(height, weight, power);
+//     this.special = special;
+//   }
+//   // getPower = () => {
+//   //   return this.power;
+//   // };
+//   getPower = () => this.power;
+// }
+
+// // const nihal = new Player(20, 12);
+// const nihal = new Player2(20, 12, 30, true);
+// console.log(nihal.getMyHeight());
+// console.log("weight", nihal.weight);
+// console.log("special", nihal.special);
+// console.log("power", nihal.getPower());
+// console.log("id", nihal.id);
